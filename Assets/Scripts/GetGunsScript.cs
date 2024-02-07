930e9ff796af51f6e86f019f41f47646577d3f73
asel <aselfernandez@gmail.com>
2024-02-07 10:44:49 +0100

    pistola azul

--- Assets/Scripts/GetGunsScript.cs
+++ Assets/Scripts/GetGunsScript.cs
@@ -18,6 +18,7 @@ namespace GetGunsScript
             gunsDict.Add(2, new Gun(0, "9mm Pistol", Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol/hl2_pistol"), Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol/hl2_pistol_viewmodel"), Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol/hl2_pistol_client")));
             gunsDict.Add(5, new Gun(1, "SPAS-12", Resources.Load<GameObject>("Prefabs/Guns/hl2_shotgun/hl2_shotgun"), Resources.Load<GameObject>("Prefabs/Guns/hl2_shotgun/hl2_shotgun_viewmodel"), Resources.Load<GameObject>("Prefabs/Guns/hl2_shotgun/hl2_shotgun_client")));
             gunsDict.Add(4, new Gun(2, "Beretta", Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol_purple/hl2_pistol_purple"), Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol_purple/hl2_pistol_purple_viewmodel"), Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol_purple/hl2_pistol_purple_client")));
+            gunsDict.Add(1, new Gun(3, "Magnum", Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol_blue/hl2_pistol_blue"), Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol_blue/hl2_pistol_blue_viewmodel"), Resources.Load<GameObject>("Prefabs/Guns/hl2_pistol_blue/hl2_pistol_blue_client")));
         }
 
         public Gun GetGun(int id)
