c6121549c50724339cb9801a053fd4621a3daaab
Iraitz7 <iraitzmauleon@gmail.com>
2024-02-08 11:03:28 +0100

    Solucionado el tema de los permisos

--- Assets/Scripts/Menu/CrearUsuario.cs
+++ Assets/Scripts/Menu/CrearUsuario.cs
@@ -9,12 +9,13 @@ public class CrearUsuario : MonoBehaviour
 {
     public GameObject mensaje;
     public string json;
-    public Respuesta respuesta;
+    public RespuestaVenta respuesta;
     public GameObject panelSesion;
     public GameObject panelRegistro;
     public GameObject crearUsuario;
     public string nombre;
     public string password;
+    public int idUsuario;
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +32,7 @@ public class CrearUsuario : MonoBehaviour
     }
 
     void PostData_Crear() => StartCoroutine(PostData_Coroutine_Crear());
+    void PutData_Permiso() => StartCoroutine(PutData_Coroutine_Permiso());
 
     IEnumerator PostData_Coroutine_Crear()
     {
@@ -64,7 +66,7 @@ public class CrearUsuario : MonoBehaviour
             else
             {
                 json = request.downloadHandler.text;
-                respuesta = JsonUtility.FromJson<Respuesta>(json);
+                respuesta = JsonUtility.FromJson<RespuestaVenta>(json);
                 if (respuesta.error != null)
                 {
                     Debug.Log(respuesta.error);
@@ -74,12 +76,52 @@ public class CrearUsuario : MonoBehaviour
                 else
                 {
                     Debug.Log(request.downloadHandler.text);
-                    mensaje.SetActive(false);
-                    panelRegistro.SetActive(false);
-                    panelSesion.SetActive(true);
-                    panelSesion.GetComponent<PostMethod>().mensaje.SetActive(true);
-                    panelSesion.GetComponent<PostMethod>().mensaje.GetComponent<TextMeshProUGUI>().text = "Usuario creado";
+                    idUsuario = respuesta.result;
+                    PutData_Permiso();
                 }
+            }
+        }
+    }
+
+    IEnumerator PutData_Coroutine_Permiso()
+    {
+        string uri = "https://retoiraitz.duckdns.org/api/res.users/";
+        //string uri = "http://localhost:8069/api/res.users/";
+
+        int permiso = 14;
+        string jsonData = "{\"sel_groups_14_15\":" + permiso + "}";
+        string jsonParams = "{\"filter\": [[\"id\", \"=\", " + idUsuario + " ]], \"data\":" + jsonData + "}";
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes("{\"params\":" + jsonParams + "}");
+        UnityWebRequest request = UnityWebRequest.Put(uri, "");
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            mensaje.SetActive(true);
+            mensaje.GetComponent<TextMeshProUGUI>().text = "Error de conexión";
+            Debug.Log(request.error);
+        }
+        else
+        {
+            json = request.downloadHandler.text;
+            RespuestaUsuario respuestaUsuario = JsonUtility.FromJson<RespuestaUsuario>(json);
+            if (respuestaUsuario.error != null)
+            {
+                mensaje.SetActive(true);
+                mensaje.GetComponent<TextMeshProUGUI>().text = "Error de conexión";
+                Debug.Log(request.error);
+            }
+            else
+            {
+                bool resultadoPut = respuestaUsuario.result;
+                mensaje.SetActive(false);
+                panelRegistro.SetActive(false);
+                panelSesion.SetActive(true);
+                panelSesion.GetComponent<PostMethod>().mensaje.SetActive(true);
+                panelSesion.GetComponent<PostMethod>().mensaje.GetComponent<TextMeshProUGUI>().text = "Usuario creado";
             }
         }
     }
